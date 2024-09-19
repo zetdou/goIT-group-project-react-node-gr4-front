93571569258053e4e40374eee3d2435fa0912467
Perslay <natalia.44.fedyk@gmail.com>
2024-09-19 13:47:36 +0200

    StoreJS and clean repo
    Preparing store.js to use future slices.
    Removing some files.
    Clean code and add colors to root in index.css.

---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import slices
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'], // to only persist the token, check out if need / don't want (blacklist) anything else
+};
+
+export const store = configureStore({
+  reducer: {
+    // write here
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
