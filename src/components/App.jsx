5421595520201b8bed24179c387a0048ee3d9b2c
Perslay <natalia.44.fedyk@gmail.com>
2024-09-19 15:09:43 +0200

    Setup redux
    Store.
    Auth selector, slice and operations with axios.
    Public folder with index.jsx.
    
    Now there is no error when entering localhost.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import { useEffect, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+// import { PrivateRoute } from './PrivateRoute';
+// import { RestrictedRoute } from './RestrictedRoute';
+// import { Layout } from './Layout/Layout';
+import { refreshUser } from '../redux/operations/auth';
+import { useAuth } from '../hooks/useAuth';
 
+// import pages like this
 // const Home = lazy(() => import('../pages/Home/Home'));
 
 export const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,8 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import slices
+import { authReducer } from './slices/authSlice';
+// import other slices
 
 const authPersistConfig = {
   key: 'auth',
@@ -20,6 +21,7 @@ const authPersistConfig = {
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     // write here
   },
   middleware: getDefaultMiddleware =>
