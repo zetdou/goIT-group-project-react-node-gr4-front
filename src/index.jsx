aad2f4f4d722a6e50103cd822d3c45b880bbbfee
Perslay <natalia.44.fedyk@gmail.com>
2024-09-20 17:41:27 +0200

    Prev basename
    Changing basename back to the previous one.

--- src/index.jsx
+++ src/index.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="goIT-group-project-react-node-gr4-front">
           <HelmetProvider>
             <App />
           </HelmetProvider>
