fdad2d1832715808aa42eb5c42387f66602f636f
Perslay <natalia.44.fedyk@gmail.com>
2024-09-22 00:52:00 +0200

    Home tablet
    Styles for medium screen.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loader } from '../Loader/Loader';
-// import { Header } from '../Header/Header';
+import { Header } from '../Header/Header';
 // import { Footer } from '../Footer/Footer';
 import css from './Layout.module.css';
 
 export const Layout = () => {
   return (
     <div className={css.container}>
-      {/* <Header /> */}
+      <Header />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,17 +3,17 @@ import googleIcon from '../../images/icons/google.svg';
 import mobileLogo from '../../images/home-images/big-logo-mobile.svg';
 import tabletLogo from '../../images/home-images/big-logo-tablet.svg';
 import desktopLogo from '../../images/home-images/big-logo-desktop.svg';
-import topMobile from '../../images/home-images/1-home-mobile.png';
-import bottomMobile from '../../images/home-images/2-home-mobile.png';
+// import topMobile from '../../images/home-images/1-home-mobile.png';
+// import bottomMobile from '../../images/home-images/2-home-mobile.png';
 import { AppBg } from '../../components/AppBg/AppBg';
 import css from './Home.module.css';
-import { Header } from '../../components/Header/Header';
+// import { Header } from '../../components/Header/Header';
 
 const Home = () => {
   return (
     <>
       <AppBg />
-      <Header />
+      {/* <Header /> */}
       <main className={css.home}>
         <Helmet>
           <title>Home</title>
