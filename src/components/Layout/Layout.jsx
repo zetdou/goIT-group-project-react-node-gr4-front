665c9377af46b1eab78d5c9198ddff14ab561f7d
Piotrek <Piotr.karys96@gmail.com>
2024-09-22 20:27:47 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loader } from '../Loader/Loader';
+
 import { Header } from '../Header/Header';
 // import { Footer } from '../Footer/Footer';
 import css from './Layout.module.css';
