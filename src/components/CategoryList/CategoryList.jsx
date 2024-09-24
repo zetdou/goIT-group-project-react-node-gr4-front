cc922b01ad04fe387cdfe62aacee3c3a0d1257f5
lukagraje <138776692+lukagraje@users.noreply.github.com>
2024-09-24 19:14:35 +0200

    Dodano CategoryList.jsx, IconsAsComponents.js, hook useReport.js

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { useReport } from '../../hooks/useReport';
+import { iconTool } from '../IconsAsComponents/IconsAsComponents';
+
+const CategoryList = ({ currentView }) => {
+  const [categoryData, setCategoryData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const reportData = useReport(currentView);
+
+  const fetchReportsData = () => {
+    try {
+      setLoading(true);
+      const categories = Object.keys(reportData || {});
+
+      const categorySum = categories.map(category => {
+        const total = Object.values(reportData[category] || {}).reduce(
+          (acc, value) => acc + value,
+          0
+        );
+        return {
+          category,
+          total: total.toString({
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          }),
+          icon: iconTool[category],
+        };
+      });
+      setCategoryData(categorySum);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchReportsData();
+  }, [currentView, reportData]);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {categoryData.map((category, index) => (
+            <li key={index}>
+              <p>{category.total}</p>
+              <div>{category.icon}</div>
+              <p>{category.category}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default CategoryList;
