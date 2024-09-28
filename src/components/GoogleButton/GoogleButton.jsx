e41539a83753bcc3573f6da14ea5c371de232b05
Perslay <natalia.44.fedyk@gmail.com>
2024-09-28 17:41:35 +0200

    Styles for button
    The issue with COOP persists.

--- src/components/GoogleButton/GoogleButton.jsx
+++ src/components/GoogleButton/GoogleButton.jsx
@@ -1,29 +1,58 @@
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+// import { GoogleLogin } from '@react-oauth/google';
 // import { jwtDecode } from 'jwt-decode';
+import googleIcon from '../../images/icons/google.svg';
+import css from './GoogleButton.module.css';
 
 export const GoogleButton = () => {
+  const login = useGoogleLogin({
+    onSuccess: credentialResponse => {
+      console.log('Success:', credentialResponse);
+      // Handle successful login
+    },
+    onError: () => {
+      console.log('Login Failed');
+    },
+    ux_mode: 'redirect', // Make sure this is set
+    redirect_uri: 'http://localhost:3000', // Make sure this matches your Google Cloud credentials
+  });
+
+  const handleClick = e => {
+    e.preventDefault();
+    login(); // Trigger Google login
+  };
+
   return (
-    <div>
-      {/* <button
-        className={css.googleButton}
-        type="button"
-        onClick={handleGoogleSignIn}
-      >
-        <img src={googleIcon} alt="google" />
-        Google
-      </button> */}
-      <GoogleLogin
-        onSuccess={credentialResponse => {
-          // const decoded = jwtDecode(credentialResponse?.credential);
-          // console.log(decoded);
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log('failed');
-        }}
-      />
-    </div>
+    <button className={css.googleButton} type="button" onClick={handleClick}>
+      <img src={googleIcon} alt="google" />
+      Google
+    </button>
   );
 };
+
+// export const GoogleButton = () => {
+//   return (
+//     <div>
+//       {/* <button
+//         className={css.googleButton}
+//         type="button"
+//         onClick={handleGoogleSignIn}
+//       >
+//         <img src={googleIcon} alt="google" />
+//         Google
+//       </button> */}
+//       <GoogleLogin
+//         onSuccess={credentialResponse => {
+//           // const decoded = jwtDecode(credentialResponse?.credential);
+//           // console.log(decoded);
+//           console.log(credentialResponse);
+//         }}
+//         onError={() => {
+//           console.log('failed');
+//         }}
+//       />
+//     </div>
+//   );
+// };
 
 // export default GoogleButton;
