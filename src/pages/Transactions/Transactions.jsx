b2dd32259b28c4668b66c4bd033f6d816fa077e0
Natalia Fedyk <natalia.44.fedyk@gmail.com>
2024-10-17 14:54:12 +0200

    Reports button
    Navigate to reports page from transactions page with a button.

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -3,19 +3,23 @@ import { Outlet } from 'react-router-dom';
 import Balance from '../../components/Balance/Balance'
 import Expenses from '../../components/Expenses/Expenses';
 import Income from '../../components/Incomes/Incomes';
+import { useNavigate } from 'react-router-dom';
 import css from './Trasactions.module.css'
 
 const App = () => {
   const [activeTab, setActiveTab] = useState('expenses');
+  const navigate = useNavigate();
 
   return (
     <div className={css.appContainer}>
-//       <div>
+      {/* <div> */}
         <Balance />
-//       </div>
+      {/* </div> */}
       <div className={css.mainContent}>
         {activeTab === 'expenses' ? <Expenses /> : <Income />}
       </div>
+
+      <button onClick={()=>navigate('/reports')}>Reports</button>
 
       <footer className={css.tabsFooter}>
         <button
